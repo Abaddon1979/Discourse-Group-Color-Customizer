f487c9d463a7efe60092c9fb031cd954a54d28a9
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2024-11-13 23:02:55 -0500

    Update 20241114034500_create_group_colors.rb

---
+++ db/migrate/20241114034500_create_group_colors.rb
@@ -0,0 +1,15 @@
+# db/migrate/20241114034500_create_group_colors.rb
+
+class CreateGroupColors
+  def change
+    create_table :group_colors do |t|
+      t.integer :group_id, null: false
+      t.string :color, null: false, default: "#000000"
+      t.integer :rank, null: false, default: 1
+
+      t.timestamps
+    end
+
+    add_index :group_colors, :group_id, unique: true
+  end
+end
