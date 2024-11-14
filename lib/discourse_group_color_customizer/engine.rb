7aca55bcc3135161a918c9f9f78fa2195d92c40b
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2024-11-13 21:35:44 -0500

    Remake

--- lib/discourse_group_color_customizer/engine.rb
+++
@@ -1,8 +0,0 @@
-# lib/discourse_group_color_customizer/engine.rb
-
-module DiscourseGroupColorCustomizer
-    class Engine < ::Rails::Engine
-      engine_name "discourse_group_color_customizer"
-      isolate_namespace DiscourseGroupColorCustomizer
-    end
-  end
