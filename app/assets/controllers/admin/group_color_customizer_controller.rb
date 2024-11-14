992274f10aea4054b6e96a58c4a37932ef9c4820
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2024-11-14 11:46:27 -0500

    asd

--- app/assets/controllers/admin/group_color_customizer_controller.rb
+++ app/assets/controllers/admin/group_color_customizer_controller.rb
@@ -1,25 +1,24 @@
 # app\assets\controllers\admin\group_color_customizer_controller.rb
 
-module ::Admin
+module Admin
   class GroupColorCustomizerController < Admin::AdminController
     requires_plugin 'discourse-group-color-customizer'
 
     def index
-      @groups = Group.order(:name)
-      @group_colors = GroupColor.all # Or whatever method to fetch displayed data
+      @groups = Group.all
+      @group_colors = GroupColor.all.index_by(&:group_id)
     end
 
     def update
       params.require(:group_colors).permit!
 
       params[:group_colors].each do |group_id, attributes|
-        group = Group.find(group_id)
-        group_color = GroupColor.find_or_initialize_by(group_id: group.id)
+        group_color = GroupColor.find_or_initialize_by(group_id: group_id)
         group_color.color = attributes[:color]
         group_color.rank = attributes[:rank].to_i
         group_color.save!
       end
-      
+
       flash[:success] = I18n.t('group_color_customizer.save')
       redirect_to admin_group_color_customizer_path
     end
