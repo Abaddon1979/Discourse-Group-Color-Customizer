23e85befe8ba855e9ce829d3a04baaa9b8865cba
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2024-11-14 12:37:35 -0500

    asd

--- app/assets/controllers/admin/group_color_customizer_controller.rb
+++ app/assets/controllers/admin/group_color_customizer_controller.rb
@@ -1,24 +1,23 @@
 # app\assets\controllers\admin\group_color_customizer_controller.rb
 
-module Admin
+module ::Admin
   class GroupColorCustomizerController < Admin::AdminController
     requires_plugin 'discourse-group-color-customizer'
 
     def index
-      @groups = Group.all
+      @groups = Group.all.order(:name)
       @group_colors = GroupColor.all.index_by(&:group_id)
+      # Ensure variables are correctly set here, serving the view
     end
 
     def update
       params.require(:group_colors).permit!
-
       params[:group_colors].each do |group_id, attributes|
         group_color = GroupColor.find_or_initialize_by(group_id: group_id)
         group_color.color = attributes[:color]
         group_color.rank = attributes[:rank].to_i
         group_color.save!
       end
-
       flash[:success] = I18n.t('group_color_customizer.save')
       redirect_to admin_group_color_customizer_path
     end
--- plugin.rb
+++ plugin.rb
@@ -9,13 +9,22 @@
 enabled_site_setting :group_color_customizer_enabled
 
 after_initialize do
-  # Example: Add custom serializers or modify existing ones
+  # Add to user serializer
   add_to_serializer(:user, :group_colors) do
     GroupColor.cached_group_colors
       .select { |group_name, _| object.groups.map(&:name).include?(group_name) }
       .map { |name, data| { name: name, color: data[:color], rank: data[:rank] } }
   end
 
-  # Load the plugin's engine if necessary
-  # load File.expand_path('lib/group_color_customizer/engine.rb', __dir__)
+  # Callback to create a corresponding GroupColor entry for each new group
+  Group.class_eval do
+    after_create :ensure_group_color_presence
+
+    def ensure_group_color_presence
+      GroupColor.where(group_id: self.id).first_or_create!(
+        color: "#000000",  # Default color
+        rank: 1            # Default rank
+      )
+    end
+  end
 end
