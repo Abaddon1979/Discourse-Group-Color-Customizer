f9817ce9bfb066686da7b3374ebbd66c1c89a410
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2024-11-14 10:59:18 -0500

    asd

--- plugin.rb
+++ plugin.rb
@@ -9,15 +9,13 @@
 enabled_site_setting :group_color_customizer_enabled
 
 after_initialize do
-  # The necessary components of your plugin are loaded by Rails and Discourse's autoload system,
-  # as the files are placed within the app directory structure.
-  
   # Example: Add custom serializers or modify existing ones
   add_to_serializer(:user, :group_colors) do
-    GroupColor.cached_group_colors.select { |group_name, _| object.groups.map(&:name).include?(group_name) }
-                                   .map { |name, data| { name: name, color: data[:color], rank: data[:rank] } }
+    GroupColor.cached_group_colors
+      .select { |group_name, _| object.groups.map(&:name).include?(group_name) }
+      .map { |name, data| { name: name, color: data[:color], rank: data[:rank] } }
   end
 
-  # Load the plugin's engine (this might be redundant if already loaded)
+  # Load the plugin's engine if necessary
   # load File.expand_path('lib/group_color_customizer/engine.rb', __dir__)
 end
