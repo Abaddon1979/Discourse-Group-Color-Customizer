c48d11408bacfc4314676a3236959d4af27bae70
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2024-11-14 20:15:02 -0500

    Delete register_admin_page.rb

--- config/locales/initializers/register_admin_page.rb
+++
@@ -1,9 +0,0 @@
-Discourse::Application.config.after_initialize do
-  if defined?(Discourse::Application::ADMIN_PAGES)
-    Discourse::Application::ADMIN_PAGES << {
-      route: 'group-color-customizer',
-      label: 'group_color_customizer.title',
-      full_url: '/admin/plugins/group-color-customizer'
-    }
-  end
-end
